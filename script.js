d63eb25a767c438f638d0ce32648de88daf16735
Git-hub-gk <161137899+Git-hub-gk@users.noreply.github.com>
2024-10-26 11:25:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const API_KEY = '3e92fe35e0aaa7695af1cd7c2de8d4a97a0a5ffa8c5b50442575ba9d2cf3a03d4b192a3763a6a59dfef1f14121a94c11';
-
+const API_KEY = '3e92fe35e0aaa7695af1cd7c2de8d4a9';
 
 function fetchAQIData(lat, lon) {
     const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
